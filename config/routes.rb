a5b4e0b43b3f770ebf3ff2cdf2108253a6ec46a5
ren.kw <renren119203@gmail.com>
2021-04-04 09:06:32 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "toppages#index"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create, :edit, :update]
 end

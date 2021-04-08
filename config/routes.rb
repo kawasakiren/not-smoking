c9b7bf004de19812b85452315f0993c3b43fad18
ren.kw <renren119203@gmail.com>
2021-04-08 07:39:44 +0000

    route 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  get 'relationships/create'
-  get 'relationships/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
   
